20a5bfa411c942535226cd20800b1c6e47e85cf7
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-12-20 18:51:38 +0100

    Trimmer legge file da input e lo mette in una variabile (funziona molto semplicemente)

--- statistics/trimmer.py
+++ statistics/trimmer.py
@@ -1,4 +1,35 @@
 # "minecraft:jump": 173914 -> "jump: 173914"
 
-def trim():
-    
+import json
+from io import open
+
+def trim(rawStat):
+    dictStat = json.loads(rawStat)
+    print(dictStat)
+    print(dictStat["nome"])
+    for key in dictStat:
+        print(key, 'corresponds to', dictStat[key])
+
+
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    if len(sys.argv) == 1:
+            print("type help as argument")
+    elif len(sys.argv) > 1:
+        if sys.argv[1] == 'help':
+            print("Usage: $ python trimmer.py <arg1>")
+            print("<arg1>: path")
+        else:
+            # Argomenti passati -----
+            path = str(sys.argv[1])
+            print("path:", str(path))
+            
+            with open(path,"r") as f:
+                contents = f.read()
+                print('Contenuto file:' + contents)
+                
+
