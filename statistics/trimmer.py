1feb9e759228d5b38aa433b430ef69ebde976a4b
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-12-21 08:48:24 +0100

    trimmer.py funzionante: implementato logging in Python; implementata BL per trimmer delle statistiche (stupidamente un bel replace)

--- statistics/trimmer.py
+++ statistics/trimmer.py
@@ -2,15 +2,32 @@
 
 import json
 from io import open
+import logging
+
+
+# Configure logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+log.addHandler(streamHandler)
+
 
 def trim(rawStat):
-    dictStat = json.loads(rawStat)
-    print(dictStat)
-    print(dictStat["nome"])
-    for key in dictStat:
-        print(key, 'corresponds to', dictStat[key])
+    '''deleting \"minecraft:\" occurency'''
+    
+    removeString = "minecraft:"
+    newString = ""
+    return rawStat.replace(removeString, newString)
+    # trimmedString = rawStat.replace(removeString, newString)
+    # dictStat = json.loads(trimmedString)
+    # print(dictStat)
+    # print(dictStat["nome"])
 
-
+    # for key in dictStat:
+    #     print(key, 'corresponds to', dictStat[key])
 
 
 if __name__ == "__main__":
@@ -18,7 +35,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) == 1:
-            print("type help as argument")
+        print("type help as argument")
     elif len(sys.argv) > 1:
         if sys.argv[1] == 'help':
             print("Usage: $ python trimmer.py <arg1>")
@@ -26,10 +43,9 @@ if __name__ == "__main__":
         else:
             # Argomenti passati -----
             path = str(sys.argv[1])
-            print("path:", str(path))
-            
-            with open(path,"r") as f:
-                contents = f.read()
-                print('Contenuto file:' + contents)
-                
+            log.info("Reading file %s", path)
 
+            with open(path, "r") as f:
+                contents = f.read()
+                log.info("Correctly readed %d characters from file %s", len(contents), path)
+                trim(contents)
